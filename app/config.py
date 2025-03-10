b6245abe93f9dc152278326d6a7164427033e355
burlak1n <roma.s-2012@yandex.ru>
2025-03-10 15:50:15 +0300

    Обновление конфигурации: добавление пустых значений по умолчанию для ключей API

--- app/config.py
+++ app/config.py
@@ -7,9 +7,9 @@ class Settings(BaseSettings):
     BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     DB_URL: str = f"sqlite+aiosqlite:///{BASE_DIR}/data/db.sqlite3"
     TOKEN: str
-    GigaChatKey: str
-    kandinsky_api_key: str
-    kandinsky_secret_key: str
+    GigaChatKey: str = ""
+    kandinsky_api_key: str = ""
+    kandinsky_secret_key: str = ""
 
     model_config = SettingsConfigDict(env_file=f"{BASE_DIR}/.env")
 
