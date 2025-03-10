5b42b2ca10be55acddd34dbfe49db800deacd10c
burlak1n <roma.s-2012@yandex.ru>
2025-03-10 15:48:19 +0300

    Рефакторинг конфигурации: оптимизация загрузки переменных окружения

--- app/config.py
+++ app/config.py
@@ -6,12 +6,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     DB_URL: str = f"sqlite+aiosqlite:///{BASE_DIR}/data/db.sqlite3"
+    TOKEN: str
+    GigaChatKey: str
+    kandinsky_api_key: str
+    kandinsky_secret_key: str
 
     model_config = SettingsConfigDict(env_file=f"{BASE_DIR}/.env")
-    TOKEN: str = model_config.get("TOKEN")
-    GigaChatKey: str = model_config.get("GigaChatKey")
-    kandinsky_api_key: str = model_config.get("kandinsky_api_key")
-    kandinsky_secret_key: str = model_config.get("kandinsky_secret_key")
 
 # Получаем параметры для загрузки переменных среды
 settings = Settings()
